33dada90ba6577046f2901e4b75bb2aa1a64073d
aurelieGabriel <gabbrielaureli7@gmail.com>
2023-10-06 14:43:56 +0200

    vignettes update

--- vignettes/EPIC.R
+++ vignettes/EPIC.R
@@ -1,17 +1,17 @@
 ## ---- eval = FALSE------------------------------------------------------------
-#  # library(EPICatac) ## If the package isn't loaded (or use EPICatac::EPIC and so on).
-#  out <- EPIC(bulk = PBMC_ATAC_data, reference = BRef_ATAC, ATAC = TRUE)
-
-## ---- eval = FALSE------------------------------------------------------------
-#  ?EPICatac::EPIC
-#  ?EPICatac::EPICatac.package
-
-## ---- eval = FALSE------------------------------------------------------------
 #  install.packages("devtools")
-#  devtools::install_github("GfellerLab/EPICatac", build_vignettes=TRUE)
+#  devtools::install_github("GfellerLab/EPIC-ATAC", build_vignettes=TRUE)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  # library(EPICatac) ## If the package isn't loaded (or use EPICatac::EPIC and so on).
+#  # library(EPICATAC) ## If the package isn't loaded (or use EPICATAC::EPIC and so on).
+#  out <- EPIC(bulk = PBMC_ATAC_data$counts, reference = BRef_ATAC, ATAC = TRUE, withOtherCells = F)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  ?EPICATAC::EPIC
+#  ?EPICATAC::EPICATAC.package
+
+## ---- eval = FALSE------------------------------------------------------------
+#  # library(EPICATAC) ## If the package isn't loaded (or use EPICATAC::EPIC and so on).
 #  counts = read.table("featureCounts.txt", skip = 1, header = T)
 #  rownames(counts) = counts$Geneid
 #  counts = counts[, c(7:ncol(counts))]
